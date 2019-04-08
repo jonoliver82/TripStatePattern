922a2c71dbd110496b3252f9d807dffbf609ac94
jonathan.oliver <jonathan.oliver@ultra-ncs.com>
2019-04-08 07:29:51 +0100

    Add project files.

---
+++ TripStatePattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripStatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
